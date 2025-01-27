2aaaa0be038c2172a40f429d507571b364baa082
NatKond <natzubova@gmail.com>
2025-01-27 10:52:45 +0100

    Add menu.java

---
+++ menu.java
@@ -0,0 +1,7 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.println("----- M E N U -----");
+		System.out.println("- Main page")
+		System.out.ptint("- Kontacts")
+	}
+}
